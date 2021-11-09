0047372d0bbb17212085229118cb24fa1dabd062
Claudio <claudiodicaprio89@gmail.com>
2021-11-09 20:49:58 +0100

    .....

--- js/script.js
+++ js/script.js
@@ -3,19 +3,18 @@ console.log(ul);
 
 let x;
 
+ 
 for (let i = 1; i <= 100; i++){
 
     if(i % 3 == 0 && i % 5 == 0){
-            x= ul.innerHTML += `<li class="box green box--${i}">${i}</li>`;
+            x = ul.innerHTML += `<li class="box green box--${i}">Fizz</li>`;
     }else if (i % 5 == 0) {
-            x=ul.innerHTML += `<li class="box yellow  box--${i}">${i}</li>`;
+            x = ul.innerHTML += `<li class="box yellow box--${i}">Buzz</li>`;
     }else if ( i % 3 == 0) {
-            x=ul.innerHTML += `<li class="box red  box--${i}">${i}</li>`;
+            x = ul.innerHTML += `<li class="box red  box--${i}">FizzBuzz</li>`;
     }else {
-            x=ul.innerHTML += `<li class="box none  box--${i}">${i}</li>`;
+            x = ul.innerHTML += `<li class="box none  box--${i}">${i}</li>`;
     }                
-}
-    
+}   
 console.log(x);
-
 
