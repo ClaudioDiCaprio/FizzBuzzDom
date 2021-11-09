ad2d083931e058ce728dc25a13ce6a4c593e8496
Claudio <claudiodicaprio89@gmail.com>
2021-11-09 23:05:54 +0100

    remove_end

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 const ul = document.querySelector('.list');
 console.log(ul);
-let x;
 
 for (let i = 1; i <= 100; i++){
     if(i % 3 == 0 && i % 5 == 0){
-            x = ul.innerHTML += `<li class="box red">Fizz</li>`;
+        ul.innerHTML += `<li class="box red">Fizz</li>`;
     }else if (i % 5 == 0) {
-            x = ul.innerHTML += `<li class="box yellow">Buzz</li>`;
+        ul.innerHTML += `<li class="box yellow">Buzz</li>`;
     }else if ( i % 3 == 0) {
-            x = ul.innerHTML += `<li class="box green ">FizzBuzz</li>`;
+         ul.innerHTML += `<li class="box green ">FizzBuzz</li>`;
     }else {
-            x = ul.innerHTML += `<li class="box none">${i}</li>`;
+         ul.innerHTML += `<li class="box none">${i}</li>`;
     }                
 };   
 
