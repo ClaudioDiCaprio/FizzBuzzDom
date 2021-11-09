83fafe0d5c5b6a513a31d9d1d64e89fef4a7cdf2
Claudio <claudiodicaprio89@gmail.com>
2021-11-09 21:37:31 +0100

    BONUS

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ let x;
 for (let i = 1; i <= 100; i++){
 
     if(i % 3 == 0 && i % 5 == 0){
-            x = ul.innerHTML += `<li class="box green box--${i}">Fizz</li>`;
+            x = ul.innerHTML += `<li class="box red box--${i}">Fizz</li>`;
     }else if (i % 5 == 0) {
             x = ul.innerHTML += `<li class="box yellow box--${i}">Buzz</li>`;
     }else if ( i % 3 == 0) {
-            x = ul.innerHTML += `<li class="box red  box--${i}">FizzBuzz</li>`;
+            x = ul.innerHTML += `<li class="box green  box--${i}">FizzBuzz</li>`;
     }else {
             x = ul.innerHTML += `<li class="box none  box--${i}">${i}</li>`;
     }                
